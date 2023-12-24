09aa5f98b78c6bff3bec08fe5f7bdddc539da014
ilnickii <ilnickii2008@gmail.com>
2023-12-24 11:10:50 +0300

    add hw ts 3

--- src/4.ts
+++ src/4.ts
@@ -1,4 +1,53 @@
-const key = new Key();
+interface IKey {
+  getSignature: () => number;
+}
+
+class Key implements IKey {
+  constructor(private signature: number) {
+    this.signature = signature;
+  }
+  getSignature(): number {
+    return this.signature;
+  }
+}
+
+class Person {
+  private key: IKey;
+  constructor(keyObj: IKey) {
+    this.key = keyObj;
+  }
+  getKey(): IKey {
+    return this.key;
+  }
+}
+
+abstract class House {
+  door: boolean;
+  key: IKey;
+  tenants: Array<Person> = [];
+  constructor(keyObj: IKey) {
+    this.key = keyObj;
+  }
+  comeIn(person: Person): void {
+    if (this.door === true) {
+      this.tenants.push(person);
+    }
+  }
+  abstract openDoor(keyObj: IKey): void;
+}
+
+class MyHouse extends House {
+  constructor(keyObj: IKey) {
+    super(keyObj);
+  }
+  openDoor(keyObj: IKey): void {
+    if (this.key.getSignature() === key.getSignature()) {
+      this.door = true;
+    }
+  }
+}
+
+const key = new Key(Math.random());
 
 const house = new MyHouse(key);
 const person = new Person(key);
@@ -6,6 +55,5 @@ const person = new Person(key);
 house.openDoor(person.getKey());
 
 house.comeIn(person);
-
 
 export {};
